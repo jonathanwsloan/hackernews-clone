202e3d4c1f8576309d6c15f4d01d69a3fd7bda3c
Kairos Garage <jonathan@kairosgarage.com>
2019-05-02 10:43:06 -0400

    (feat)Add error handling

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -74,7 +75,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
 
@@ -105,7 +106,8 @@ class App extends Component {
     const {
       searchTerm,
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
     const page = (
       results &&
@@ -128,6 +130,14 @@ class App extends Component {
             Search
           </Search>
         </div>
+        {error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />}
         <Table
           list={list}
           onDismiss={this.onDismiss}
